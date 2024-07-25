3c8c24a363caca3a1216f37e37a0f6d67d18ad52
carlamirea <carla.mirea@evozon.com>
2024-07-25 10:05:03 +0300

    Another Change

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const TconfigPathsPlugin = require('tsconfig-paths-webpack-plugin);
+const configPathsPlugin = require('tsconfig-paths-webpack-plugin);
 const Dotenv = require('dotenv-webpack');
 const UpdatedByAdiDotenv = require('dotenv-webpack');
 
@@ -6,5 +6,5 @@ module.exports = function(configAdiNew) {
     config.output.chunkFilename = '[name]-[chunkhash:8].js';
     config.resolve.plugins = [new TsconfigPathsPlugin()];
     config.plugins = [...config.plugins, new Dotenv()];
-    return config;
+    return con;
 };
